c8b9e9ac17099a3160a297c24e7eb02586a9c7f5
Nasimuzzaman <md.nasimuzzaman@g.bracu.ac.bd>
2025-01-01 22:24:17 +0600

    Developing registration repository

---
+++ src/main/java/com/dev_nasimuzzaman/services/UsersTypeService.java
@@ -0,0 +1,22 @@
+package com.dev_nasimuzzaman.services;
+
+import com.dev_nasimuzzaman.entity.UsersType;
+import com.dev_nasimuzzaman.repository.UsersTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UsersTypeService {
+
+    private final UsersTypeRepository usersTypeRepository;
+
+    @Autowired
+    public UsersTypeService(UsersTypeRepository usersTypeRepository) {
+        this.usersTypeRepository = usersTypeRepository;
+    }
+    public List<UsersType> getAll(){
+        return usersTypeRepository.findAll();
+    }
+}
