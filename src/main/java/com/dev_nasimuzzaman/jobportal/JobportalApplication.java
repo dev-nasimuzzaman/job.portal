13c8fa0553a18c88b7154fecd4c4559acbe72678
Nasimuzzaman <md.nasimuzzaman@g.bracu.ac.bd>
2025-01-01 11:45:35 +0600

    job portal application

---
+++ src/main/java/com/dev_nasimuzzaman/jobportal/JobportalApplication.java
@@ -0,0 +1,13 @@
+package com.dev_nasimuzzaman.jobportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobportalApplication.class, args);
+	}
+
+}
