523f477c5832d541f111488001d05c0966bc4afd
Nasimuzzaman <md.nasimuzzaman@g.bracu.ac.bd>
2025-01-03 02:31:46 +0600

    Developing registration repository and controller

--- src/main/java/com/dev_nasimuzzaman/controller/UsersController.java
+++ src/main/java/com/dev_nasimuzzaman/controller/UsersController.java
@@ -2,20 +2,27 @@ package com.dev_nasimuzzaman.controller;
 
 import com.dev_nasimuzzaman.entity.Users;
 import com.dev_nasimuzzaman.entity.UsersType;
+import com.dev_nasimuzzaman.services.UsersService;
 import com.dev_nasimuzzaman.services.UsersTypeService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
 @Controller
 public class UsersController {
     private final UsersTypeService usersTypeService;
+    private final UsersService usersService;
+
+
     @Autowired
-    public UsersController(UsersTypeService usersTypeService) {
+    public UsersController(UsersTypeService usersTypeService, UsersService usersService) {
         this.usersTypeService = usersTypeService;
+        this.usersService = usersService;
     }
     @GetMapping("/register")
     public String register(Model model){
@@ -23,5 +30,11 @@ public class UsersController {
        model.addAttribute("getAllTypes",usersTypes);
        model.addAttribute("user",new Users());
        return "register";
+    }
+    @PostMapping("/register/new")
+    public String userRegistration(@Valid Users users){
+    //    System.out.println("User:: "+users);
+        usersService.addNew(users);
+        return "dashboard";
     }
 }
