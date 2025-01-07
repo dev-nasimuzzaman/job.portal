9ac8e5097d957455450b03c1b1c1cad8d11090a1
Nasimuzzaman <md.nasimuzzaman@g.bracu.ac.bd>
2025-01-08 02:12:17 +0600

    CustomAuthenticationSuccessHandler

--- src/main/java/com/dev_nasimuzzaman/repository/UsersRepository.java
+++ src/main/java/com/dev_nasimuzzaman/repository/UsersRepository.java
@@ -1,7 +1,10 @@
 package com.dev_nasimuzzaman.repository;
 
 import com.dev_nasimuzzaman.entity.Users;
+import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface UsersRepository extends JpaRepository<Users,Integer> {
     Optional<Users> findByEmail(String email);
