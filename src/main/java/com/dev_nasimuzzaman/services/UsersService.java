8f0dc34423f2fff6a2f0374fdf04a960f5ef50da
Nasimuzzaman <md.nasimuzzaman@g.bracu.ac.bd>
2025-01-08 22:31:25 +0600

    Integrate Dashboard with Current Logged In User

--- src/main/java/com/dev_nasimuzzaman/services/UsersService.java
+++ src/main/java/com/dev_nasimuzzaman/services/UsersService.java
@@ -7,6 +7,11 @@ import com.dev_nasimuzzaman.repository.JobSeekerProfileRepository;
 import com.dev_nasimuzzaman.repository.RecruiterProfileRepository;
 import com.dev_nasimuzzaman.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -40,6 +45,25 @@ public class UsersService {
         }
 
         return savedUser;
+    }
+
+    public Object getCurrentUserProfile() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(!(authentication instanceof AnonymousAuthenticationToken)){
+            String username = authentication.getName();
+            Users users = usersRepository.findByEmail(username).orElseThrow(()-> new
+                    UsernameNotFoundException("Could not found" + "user"));
+            int userId = users.getUserId();
+            if(authentication.getAuthorities().contains(new SimpleGrantedAuthority
+                    ("Recruiter"))){
+            RecruiterProfile recruiterProfile = recruiterProfileRepository.findById(userId).orElse(new RecruiterProfile());
+            return recruiterProfile;
+            } else {
+                JobSeekerProfile jobSeekerProfile = jobSeekerProfileRepository.findById(userId).orElse(new JobSeekerProfile());
+                return jobSeekerProfile;
+            }
+        }
+        return null;
     }
 }
 
