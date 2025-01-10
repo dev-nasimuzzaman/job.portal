76497a1dea45720701814b57ad0ecd2a7a8062ad
Nasimuzzaman <md.nasimuzzaman@g.bracu.ac.bd>
2025-01-10 07:27:34 +0600

    Add login logout Request Mapping to Controller

--- src/main/java/com/dev_nasimuzzaman/controller/UsersController.java
+++ src/main/java/com/dev_nasimuzzaman/controller/UsersController.java
@@ -4,8 +4,13 @@ import com.dev_nasimuzzaman.entity.Users;
 import com.dev_nasimuzzaman.entity.UsersType;
 import com.dev_nasimuzzaman.services.UsersService;
 import com.dev_nasimuzzaman.services.UsersTypeService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -36,5 +41,18 @@ public class UsersController {
     //    System.out.println("User:: "+users);
         usersService.addNew(users);
         return "dashboard";
+    }
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response){
+        Authentication authentication = SecurityContextHolder.getContext()
+                .getAuthentication();
+        if(authentication!=null){
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+        return "redirect:/";
     }
 }
